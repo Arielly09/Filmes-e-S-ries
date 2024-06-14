28321d4ecba7951a1a20c85a189c9b9a1bdb6edc
Arielly09 <162440483+Arielly09@users.noreply.github.com>
2024-06-14 08:32:28 -0300

    Add files via upload

--- modelo.py
+++ modelo.py
@@ -1,28 +1,27 @@
-class Filme:
+class Filme(Programa    ):
     def __init__(self, nome, ano, duracao):
-        self.nome = nome.title()
+        self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.likes = 0
+        self.__likes = 0
 
-        def dar_like(self):
-            self.likes += 1
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporada):
-        self.nome = nome
+        self.__nome = nome
         self.ano = ano
         self.temporada = temporada 
-        self.likes = 0
-
-    def dar_likes(self):
-        self.likes += 1
-
+        self.__likes = 0
+      
 vingadores = Filme('Vingadores - Guerra Infinita', 2018, 160)
+vingadores.dar_likes()
 print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
 
+
 atlanta = Serie('Atlanta', 218, 2)
+atlanta.dar_likes()
+atlanta.dar_likes()
 
 print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.Temporadas}')
 
-atlanta = Serie('atlanta', 2018, 2)
+def dar_likes(self):
+        self.likes += 1
